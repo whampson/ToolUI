0dfe79d3f4666223dec91dc8c71d3fac517c17c1
Wes Hampson <thehambone93@gmail.com>
2020-09-26 14:27:14 -0700

    Add DialogCloseEventArgs

---
+++ ToolUI/Events/DialogCloseEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WHampson.ToolUI.Events
+{
+    public class DialogCloseEventArgs : EventArgs
+    {
+        public bool? DialogResult { get; set; }
+
+        public DialogCloseEventArgs(bool? dialogResult = null)
+        {
+            DialogResult = dialogResult;
+        }
+    }
+}
